ff0fb30f22b32690b809ff9b9c00be657d1da240
Anuki Pasqual <anukipasqual@gmail.com>
2022-12-30 05:20:11 +0530

    Started vectorizing functions

--- algorithm/hazard_detect.py
+++ algorithm/hazard_detect.py
@@ -19,22 +19,39 @@ def inverse_gamma_transform(signal: float) -> float:
         return signal / 12.92
     else:
         return ((signal + 0.055) / 1.055) ** 2.4
+    
+    
+# vectorized versions
+def gamma_transform(signal: np.ndarray) -> np.ndarray:
+    """Nonlinear electro-optical transfer function to convert a 
+    signal from linear color space to sRGB color space."""
+    
+    return np.where(signal <= 0.0031308, signal * 12.92, 1.055 * (signal ** (1 / 2.4)) - 0.055)
+
+
+def inverse_gamma_transform(signal: np.ndarray) -> np.ndarray:
+    """Nonlinear electro-optical transfer function to convert a
+    signal from sRGB color space to linear color space."""
+    
+    return np.where(signal <= 0.03928, signal / 12.92, ((signal + 0.055) / 1.055) ** 2.4)
 
 
 def linearRGB_to_sRGB(linear: np.ndarray) -> np.ndarray:
     """Convert linear RGB to sRGB"""
-    sRGB = np.array([gamma_transform(linear[0]), gamma_transform(
-        linear[1]), gamma_transform(linear[2])])
+    #sRGB = np.array([gamma_transform(linear[0]), gamma_transform(
+        #linear[1]), gamma_transform(linear[2])])
+    sRGB = gamma_transform(linear)
     return sRGB
 
 
 def sRBG_to_linearRGB(sRGB: np.ndarray) -> np.ndarray:
     """Convert sRGB to linear RGB"""
-    linear = np.array([inverse_gamma_transform(sRGB[0]), inverse_gamma_transform(
-        sRGB[1]), inverse_gamma_transform(sRGB[2])])
+    # linear = np.array([inverse_gamma_transform(sRGB[0]), inverse_gamma_transform(
+    #     sRGB[1]), inverse_gamma_transform(sRGB[2])])
+    linear = inverse_gamma_transform(sRGB)
     return linear
 
-
+# Fix these indices
 def linearRBG_to_Ls(linearRGB: np.ndarray) -> float:
     """Compute the relative luminance value of a pixel in sRGB color space."""
 
@@ -48,27 +65,31 @@ def red_ratio(sRGB: np.ndarray) -> float:
 
 def pure_red(sRGB: np.ndarray) -> float:
     """Compute the pure red of a color in sRGB color space."""
-    if sRGB[0] > sRGB[1] + sRGB[2]:
-        return (sRGB[0] - sRGB[1] - sRGB[2]) * 320
-    return 0
+    # if sRGB[0] > sRGB[1] + sRGB[2]:
+    #     return (sRGB[0] - sRGB[1] - sRGB[2]) * 320
+    # return 0
+    comp = (sRGB[0] - sRGB[1] - sRGB[2])
+    return np.where(comp > 0, comp * 320, 0)
 
 
-def is_luminance_flash(Ls: float, prev_Ls: float) -> bool:
+def is_luminance_flash(Ls: np.ndarray, prev_Ls: np.ndarray) -> np.ndarray:
     """Returns True if the relative luminance values are different enough to be considered a flash."""
 
-    brigher_Ls = max(Ls, prev_Ls)
-    darker_Ls = min(Ls, prev_Ls)
+    brigher_Ls = np.max([Ls, prev_Ls], axis = 0)
+    darker_Ls = np.min([Ls, prev_Ls], axis = 0)
 
-    return (brigher_Ls - darker_Ls) / brigher_Ls >= 0.1 and darker_Ls < 0.8
+    return np.all([(brigher_Ls - darker_Ls) / brigher_Ls >= 0.1, darker_Ls < 0.8], axis = 0)
 
 
-def is_saturated_red_flash(linear_color: np.ndarray, prev_linear_color: np.ndarray) -> bool:
+def is_saturated_red_flash(linear_color: np.ndarray, prev_linear_color: np.ndarray) -> np.ndarray:
     """Returns True if the the transition is a saturated red flash."""
-    if red_ratio(linear_color) >= 0.8 or red_ratio(prev_linear_color) >= 0.8:
-        if abs(pure_red(linear_color) - pure_red(prev_linear_color)) >= 20:
-            return True
+    # if red_ratio(linear_color) >= 0.8 or red_ratio(prev_linear_color) >= 0.8:
+    #     if abs(pure_red(linear_color) - pure_red(prev_linear_color)) >= 20:
+    #         return True
 
-    return False
+    # return False
+    return np.all([np.any([red_ratio(linear_color) >= 0.8, red_ratio(prev_linear_color) >= 0.8]), \
+        abs(pure_red(linear_color) - pure_red(prev_linear_color)) >= 20])
 
 
 def is_hazardous(color: np.ndarray, prev_color: np.ndarray) -> bool:
@@ -91,3 +112,24 @@ def is_hazardous(color: np.ndarray, prev_color: np.ndarray) -> bool:
         return True
 
     return False
+
+def is_hazardous(color: np.ndarray, prev_color: np.ndarray) -> np.ndarray:
+    """Returns True if the transition is a hazard."""
+
+    # Transform to linear
+    linear_color = sRBG_to_linearRGB(color)
+    linear_prev_color = sRBG_to_linearRGB(prev_color)
+
+    # Compute the relative luminance value
+    prev_Ls = linearRBG_to_Ls(linear_prev_color)
+    Ls = linearRBG_to_Ls(linear_color)
+
+    # If the relative luminance values are different enough to be considered a flash
+    # or If a Saturated Red Flash is detected
+    return np.any([is_luminance_flash(Ls, prev_Ls), is_saturated_red_flash(linear_color, linear_prev_color)])
+
+
+# color1 = np.array([[1, 1, 1], [0, 0, 0]])
+# color2 = np.array([[0, 0, 0.1], [0, 0, 0]])
+# print(is_luminance_flash(color1, color2))
+        
--- algorithm/main.py
+++ algorithm/main.py
@@ -12,11 +12,13 @@ cap = cv2.VideoCapture(0)
 # Disable auto exposure
 # TODO: This is not working
 
+# Reduce camera resolution
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_WIDTH//4)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FRAME_HEIGHT//4)
 
 # Width and height of the webcam frame
 WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
 
 # Set the quarter area threshold (in pixels)
 quarter_area_threshold = (WIDTH * HEIGHT) / 4
@@ -29,34 +31,42 @@ prev_time = time.time()
 
 
 # Capture frames from the webcam
-while True:
-    # Read a frame from the webcam
-    ret, frame = cap.read()
-
-    # If the frame was successfully read
-    if ret:
-        # Add the frame to the frame buffer
-        frame_buffer.append(frame)
-
-        # If the frame buffer has at least 8 frames
-        if len(frame_buffer) >= BUFFER_SIZE:
-
-            # Get the current FrameSlidingWindow
-            frame_sliding_window = frame_buffer[-BUFFER_SIZE:]
-
-            # For each pixel location in frame
-            for j in range(1, BUFFER_SIZE):
-                # Initialize the flashing pixels count
-                flashing_pixels_count = 0
-                # Get the current and previous frames
-                cur_frame = frame_sliding_window[j]
-                prev_frame = frame_sliding_window[j-1]
-
-                # For each pixel in frame
-                for k in range(frame.shape[0]):  # TODO: This is too slow
-                    for l in range(frame.shape[1]):
-                        if is_hazardous(cur_frame[k][l] / 255, prev_frame[k][l] / 255):
-                            flashing_pixels_count += 1
-
-                if (flashing_pixels_count > quarter_area_threshold):
-                    print("Flashing detected")
+try:
+    while True:
+        # Read a frame from the webcam
+        ret, frame = cap.read()
+    
+        # If the frame was successfully read
+        if ret:
+            # Add the frame to the frame buffer
+            frame_buffer.append(frame)
+    
+            # If the frame buffer has at least 8 frames
+            if len(frame_buffer) >= BUFFER_SIZE:
+    
+                # Get the current FrameSlidingWindow
+                frame_sliding_window = frame_buffer[-BUFFER_SIZE:]
+    
+                # For each pixel location in frame
+                for j in range(1, BUFFER_SIZE):
+                    # Initialize the flashing pixels count
+                    flashing_pixels_count = 0
+                    # Get the current and previous frames
+                    cur_frame = frame_sliding_window[j]
+                    prev_frame = frame_sliding_window[j-1]
+    
+                    # For each pixel in frame
+                    # for k in range(frame.shape[0]):  # TODO: This is too slow
+                    #     for l in range(frame.shape[1]):
+                    #         if is_hazardous(cur_frame[k][l] / 255, prev_frame[k][l] / 255):
+                    #             flashing_pixels_count += 1
+                    flashing_pixels_count = np.sum(is_hazardous(cur_frame / 255, prev_frame / 255))
+                    if (flashing_pixels_count > quarter_area_threshold):
+                        print("Flashing detected")
+                        
+                print("Frame processed")
+                
+except KeyboardInterrupt:
+    cap.release()
+    cv2.destroyAllWindows()
+    
